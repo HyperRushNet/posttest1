cd2e8cfb2bee36011a6c75d2745810319ebde049
HyperRushNet <hyperrushnetwork@gmail.com>
2025-02-22 21:58:36 +0100

    Update post.js

--- api/post.js
+++ api/post.js
@@ -1,6 +1,14 @@
 export default function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // Hiermee haal je CORS weg
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end(); // Reageer op OPTIONS-verzoek (CORS preflight)
+    }
+
     if (req.method === "POST") {
-        res.json({ message: "Data ontvangen", data: req.body });
+        res.json({ message: "POST ontvangen!", data: req.body });
     } else {
         res.status(405).json({ error: "Alleen POST toegestaan" });
     }
